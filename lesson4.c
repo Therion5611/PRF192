07d484b555ac12ed3f390ca5f857169c88abf3fa
Therion5611 <vinhquangdoan0@gmail.com>
2025-10-07 23:44:34 +0700

    Update lesson4.c

--- lesson4.c
+++ lesson4.c
@@ -1,10 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h> 
 
+int reverse(int n){
 
-
-int main() {
-	
-	printf("hello world");
-	return 0;
+	bool kt6 = false; 
+	int temp = 0;
+	int m = n;
+	int sum = 0;
+	while(n) {
+		temp *= 10;
+		temp += n%10;
+		sum += temp;
+		if (n%10 == 6){
+			kt6 = true;
+		}
+		n /= 10;
+	} 
+	return m == temp && kt6 && sum%10 == 8; 
 }
+
+
+int main(){
+	int a, b;
+	
+	printf("Nhap vao so :\na = ");
+	scanf("%d", &a);
+	printf("b = ");
+	scanf("%d", &b);
+	
+	for (int i = a; i <= b; i++) {
+		if (reverse(i)) {
+			printf("%d ", i);
+		}
+	}
+}
+
+
