425ad2ec006b338aee1c32771d4731d8d4d8058f
Therion5611 <vinhquangdoan0@gmail.com>
2025-10-21 17:08:34 +0700

    Add ex1.c

---
+++ ex1.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+
+int sphenic(int n) {
+	int res = 0;
+	int count = 0;
+	while (n%2 == 0) {
+		count++;
+		n /= 2;
+	}
+	if (count >= 2) return 0;
+    if (count == 1) res++;
+	
+	for (int i = 2; i <= sqrt(n); i++) {
+		int count = 0;
+		while(n%i==0) {
+			count++;
+			n/=i;
+		}
+		if (count >= 2) return 0;
+		if (count == 1) res++;
+	}
+	if (n > 1) res++;
+	return res == 3;
+	
+}
+
+int main(){
+	int n;
+	printf("Nhap vao so n : ");
+	scanf("%d", &n);
+	
+	int result = sphenic(n);
+	
+	(result == 1) ? printf("%d", result) : printf("0"); 
+	
+	return 0;
+} 
